f8ca0359a3f4a53f9e99e02fda150916138d4767
raistlinqjx <raistlinqjx@gmail.com>
2017-12-14 22:46:02 +0800

    RollBall Finish
    Finish!

--- RollBall2/Assets/Scripts/PlayerController.cs
+++ RollBall2/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
+	private int count;
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	void Start(){
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText ();
+		winText.text = "";
 	}
 	void Update(){
 	}
@@ -18,5 +25,21 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed );
+	}
+
+	void OnTriggerEnter(Collider other) {
+//		Destroy(other.gameObject);
+		if (other.gameObject.CompareTag("PickUp")){
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count:" + count.ToString ();
+		if (count >= 13) {
+			winText.text = "you win";
+		}
 	}
 }
